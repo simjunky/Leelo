58d727237c5a24dc5a557d3525f67ce6c17737e5
simjunky <patrick.martin.2@outlook.de>
2022-08-17 12:35:57 +0200

    next step in deploying to github pages not yet functional

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,12 @@
-using Documenter, Leelo
+using Leelo, Documenter
 
 makedocs(sitename = "Leelo.jl",
     pages = ["index.md", "manual.md", "documentation.md"])
 
-deploydocs(repo = "github.com/simjunky/Leelo.git")
+
+ENV["GITHUB_EVENT_NAME"] = "push"
+ENV["GITHUB_REPOSITORY"] = "github.com/simjunky/Leelo.git"
+ENV["GITHUB_REF"] = "master"
+
+
+deploydocs(repo = "github.com/simjunky/Leelo.git", target = "build", deps = nothing, make = nothing, devbranch = "master", versions = nothing, deploy_config = Documenter.GitHubActions())
